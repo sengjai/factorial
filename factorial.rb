4ab2759a4ce8553fddfbdd97f3d8506d50a8511b
Rudzainy_Rahman <rudzainy@gmail.com>
2016-05-11 04:50:58 -0400

    Add factorial.rb

---
+++ factorial.rb
@@ -0,0 +1,9 @@
+# Implement an iterative version of the factorial function
+def factorial_iterative(n)
+end
+
+# Implement a recursive version of the factorial function
+def factorial_recursive(n)
+end
+
+puts factorial_recursive(9) == 362880
